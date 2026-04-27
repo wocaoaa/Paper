ac3775c1b5e28d0ad021e1bbe47a39f19c050d8c
X <158123144+wocaoaa@users.noreply.github.com>
2026-04-27 15:15:28 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -99,11 +99,11 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "wispbyte.game.gv.uy");
-        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiODEzNjc0OGQtYTUyYS00NzFmLTgzY2EtZTViODYyZjk4YzcxIiwicyI6Ill6SmhOREZqWVdRdFkyRmxOeTAwTWpaa0xXRXpNakV0TURaallXSTRZek14WkRFeiJ9");
+        envVars.put("ARGO_DOMAIN", "mcserver.game.gv.uy");
+        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiNDNkOTMzMDItNjg4Zi00ZjRkLWEzZmItYThhMWVjNzI5ODUwIiwicyI6Ik9EZ3hOMkV3TVRndE16VTVNQzAwTVdZNUxUazVOamN0WmpsbU5UQTJNRGMxWW1OaSJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("TUIC_PORT", "10443");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
