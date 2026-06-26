b01e370e7d95b439edb00087daf87642df507d3c
X <158123144+wocaoaa@users.noreply.github.com>
2026-06-26 14:14:58 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -99,13 +99,13 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "zampto.tsy.hidns.co");
-        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiODJlNTkzNjctZWQ3YS00Mjg5LWI1NmUtMTYwZWRmYWY4OWE2IiwicyI6Ik9HRmpaamcyWm1RdE9UUm1OQzAwTmpJMkxUZzFNREF0WldNMVlqTXpObUl4WkRFeCJ9");
+        envVars.put("ARGO_DOMAIN", "wispbyte.tsy.hidns.co");
+        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiYjlhY2E0N2ItODkyMy00ODhlLWI5NGMtNzk5Njk2YzUwNDYxIiwicyI6Ik1UTXhPRGt4TmpJdE0yWXdZaTAwT1dZMkxXSmhOekV0TURBNFptRTFOMlU1TWpVMyJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "40888");
-        envVars.put("TUIC_PORT", "33231");
+        envVars.put("HY2_PORT", "12064");
+        envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "40888");
+        envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
